1c56003702a1f7cc62782bf8c2a75eca03308bdd
André Costa <andre.costa@ess.eu>
2025-01-07 15:01:52 +0100

    More improvements

--- .ci_functions.sh
+++ .ci_functions.sh
@@ -1,18 +1,21 @@
 # Functions for GitLab CI
 setup_conan() {
+    local conan_name=$1
+    local conan_url=$2
+
     echo 'Setting up Conan...'
-    conan remote add "$conan_remote_name" "$conan_remote_url" --force
+    conan remote add "$conan_name" "$conan_url" --force
     conan user --password "$ESS_ARTIFACTORY_ECDC_CONAN_TOKEN" --remote ecdc-conan-external "$ESS_ARTIFACTORY_ECDC_CONAN_USER"
 }
 
 upload_packages_if_target_container() {
   local current_container=$1
-  local target_container=$2
+  local target_upload_container=$2
   local conan_user=$3
   local conan_pkg_channel=$4
   local conan_file_path=$5
 
-  if [[ "$current_container" == "$target_container" ]]; then
+  if [[ "$current_container" == "$target_upload_container" ]]; then
     packageNameAndVersion=$(conan inspect --attribute name --attribute version $conan_file_path | awk -F': ' '{print $2}' | paste -sd'/')
     conan upload --all --no-overwrite --remote ecdc-conan-external ${packageNameAndVersion}@${conan_user}/${conan_pkg_channel}
   fi
